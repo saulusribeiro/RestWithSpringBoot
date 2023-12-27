4e72ed2b73614a0da8759ca74eeb5b7d81dc758e
unknown <saulus.ribeiro@mv.com.br>
2023-12-27 17:19:20 -0300

    First Steps With Spring Boot

---
+++ 05_FirstStepsWithSpringBoot/RestWithSpringBoot/src/main/java/br/com/srdeveloper/StartUp.java
@@ -0,0 +1,13 @@
+package br.com.srdeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartUp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartUp.class, args);
+	}
+
+}
